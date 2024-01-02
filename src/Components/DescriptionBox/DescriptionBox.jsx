778e54337f58864fb0bcfb9c7b887643f39f5de2
Devashish011 <devashishrathore2019@gmail.com>
2024-01-02 16:32:04 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Revievs (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+         Discover the epitome of style at our online clothing
+         emporium. Immerse yourself in a curated collection
+         of fashion-forward attire that transcends trends. 
+         Explore aspectrum of garments, from timeless classics to contemporary chic, meticulously
+         selected for your discerning taste. Our user-friendly
+         platform ensures a seamless shopping experience, 
+         complete with detailed product descriptions and 
+         high-quality images. Elevate your wardrobe effortlessly
+         with our diverse range of clothing, where fashion 
+         meets convenience. Unveil a world of sartorial 
+         excellence with just a click - because your style 
+         journey begins here.Indulge in a seamless shopping experience 
+         with secure transactions and swift deliveries. Embrace
+          effortless style with our clothing range, where 
+          every purchase reflects your unique fashion
+           sensibilities.
+         </p>
+         <p>
+         Navigate our eCommerce haven with confidence, enjoying hassle-free returns and responsive customer support. Immerse yourself in a world of sartorial delight, where curated fashion meets convenience at every click.
+
+         </p>
+      </div>
+    </div>
+  )
+}
+export default DescriptionBox 
---
+++ src/Pages/cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CartItems from '../Components/CartItems/CartItems'
+const cart = () => {
+  return (
+    <div>
+       <CartItems/>
+    </div>
+  )
+}
+
+export default cart
